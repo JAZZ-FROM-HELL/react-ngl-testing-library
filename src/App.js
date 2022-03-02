937b4ba19dc2b04b9fd7a35637a9a1040d3fd37c
yuval <yuval.ron.services@gmail.com>
2022-03-02 09:09:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useMemo} from 'react';
+import {Component, Stage} from 'react-ngl';
 
 function App() {
+
+  const reprList = useMemo(() => [{
+    type: 'cartoon'
+  }], []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div role='app' className='App'>
+        <div className='model-viewport-container'>
+          <Stage width='600px' height='400px'>
+            <Component path='rcsb://4hhb' reprList={reprList} />
+          </Stage>
+        </div>
+      </div>
   );
 }
 
